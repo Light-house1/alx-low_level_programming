050627d530b0a51c3c7ef785a0e62aea2083f773
Onyema Justice <onyemajustice@gmail.com>
2022-06-03 09:11:26 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+	char achar;
+	int figure;
+	long along;
+	long long alongfigure;
+	float floatfigure;
+	
+	printf("Size of a char: %lu byte(s)\n", sizeof(achar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(figure));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alongfigure));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floatfigure));
+	return(0);
+}
