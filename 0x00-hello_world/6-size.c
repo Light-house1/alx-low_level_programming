2bfee0977456cc620cc7e114b5d861e4688e6254
Onyema Justice <onyemajustice@gmail.com>
2022-06-04 11:04:18 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char achar;
@@ -6,11 +11,12 @@ int main(void)
 	long along;
 	long long alongfigure;
 	float floatfigure;
-	
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(achar));
 	printf("Size of an int: %lu byte(s)\n", sizeof(figure));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alongfigure));
 	printf("Size of a float: %lu byte(s)\n", sizeof(floatfigure));
-	return(0);
+
+	return (0);
 }
