05b531d72308c0cefce26eb8320d2e3d6f46af67
Onyema Justice <onyemajustice@gmail.com>
2022-06-10 13:37:38 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/** Fizz-Buzz test” is an interview question
+ *  that prints the numbers from 1 to 100
+*/
+int main()
+{
+	int c;
+	for (c = 1; c <= 100; c++)
+	{
+		if (c % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		if (c % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		if (c % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+			printf("%d ",c);
+
+	}
+	return (0);
+}
