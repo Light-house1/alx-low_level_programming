546ca2abaafecf76cb26a05f30925fa2321f9dc5
Onyema Justice <onyemajustice@gmail.com>
2022-06-22 11:52:58 -0700

    correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * _print_rev_recursion -  prints a string, in reverse.
- * @s: pointer to string
- * Return: nothing
- **/
-void _print_rev_recursion(char *s)
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
+ */
+int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
