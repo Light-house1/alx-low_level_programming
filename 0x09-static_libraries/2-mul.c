5ece7f931fca939cc9290d1deca70fafbdae57a7
Onyema Justice <onyemajustice@gmail.com>
2022-06-27 07:42:42 -0700

    status

---
+++ 0x09-static_libraries/2-mul.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * mul - check the code
+ *@a : first integer
+ *@b : second integer
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	return (x);
+}
